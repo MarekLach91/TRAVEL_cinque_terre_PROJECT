efab81d64b225437deee7b1e328e2200d24f3756
Marek Lach <marek.lach91@gmail.com>
2017-05-10 17:49:13 +0200

    10.05

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+var map = document.querySelector('.map');
+console.log(map);
+
+
+
+//$('.map')
+//	.click(function(){
+//			$(this).find('iframe').addClass('map_scroll')})
+//	.mouseleave(function(){
+//			$(this).find('iframe').removeClass('map_scroll')});
+////
+//onClick="style.pointerEvents='none'"
+    
+});
