0c0b5e1bb4a0e7b446fb7915000d7e0591042bab
Marek Lach <marek.lach91@gmail.com>
2017-05-18 11:53:25 +0200

    18.05

--- js/app.js
+++ js/app.js
@@ -1,36 +1,16 @@
-//document.addEventListener("DOMContentLoaded", function(event) {
-//
-//var map = document.querySelector('.map');
-//console.log(map);
-
-
 $(document).ready(function() { 
  
-	$('a[href^="#"]').on('click', function(event) {
-	
-		var target = $( $(this).attr('href') );
-	
-		if( target.length ) {
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top
-			}, 1000);
-		}
-	});
- 
-});
+    
+    
+      $('.map').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('.map').on('click', function () {
+            $('#map').removeClass('scrolloff'); // set the pointer events true on click
+        });
     
     
     
     
-    
-//$('.map')
-//	.click(function(){
-//			$(this).find('iframe').addClass('map_scroll')})
-//	.mouseleave(function(){
-//			$(this).find('iframe').removeClass('map_scroll')});
-////
-//onClick="style.pointerEvents='none'"
+
     
     
     
