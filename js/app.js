a3c74d23554f38dc10c97ea79b65fea79c326fdc
Marek Lach <marek.lach91@gmail.com>
2017-05-18 15:41:49 +0200

    18.05 5

--- js/app.js
+++ js/app.js
@@ -4,17 +4,8 @@ $(document).ready(function() {
     
       $('.map').addClass('scrolloff'); // set the pointer events to none on doc ready
         $('.map').on('click', function () {
-            $('#map').removeClass('scrolloff'); // set the pointer events true on click
+            $('.map').removeClass('scrolloff'); // set the pointer events true on click
         });
-    
-    
-    
-    
 
-    
-    
-    
-    
-    
     
 });
